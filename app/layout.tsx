85dce0adeece1aa34fac9bbe7bc181b7445b2f47
sidpandagle <adi.pandagle@gmail.com>
2024-03-25 00:41:55 +0530

    Canonical Duplication changes done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import './globals.css'
-import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -7,16 +6,6 @@ const inter = Inter({ subsets: ['latin'] })
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 import { GoogleTagManager } from '@next/third-parties/google'
- 
-
-export const metadata: Metadata = {
-  title: 'Congruence Market Insights - Market Research & Consulting',
-  description: 'Congruence is your trusted partner in navigating emerging opportunities and challenges through high-quality, tailored market research reports.',
-  keywords: 'Congruence Market Insights, CMI, Market Research, Industry Analysis, Market Analysis, Global Market Trends, Business Intelligence, Custom Market Research, Strategic Insights, Market Dynamics, Emerging Opportunities, Competitive Landscape, Data-driven Decision Making, Market Forecast, Industry Reports, Tailored Research Solutions, Market Intelligence Services',
-  alternates: {
-    canonical: `https://www.congruencemarketinsights.com/`,
-  },
-}
 
 export default function RootLayout({
   children,
--- app/page.tsx
+++ app/page.tsx
@@ -8,10 +8,20 @@ import Insights from '@/components/Insights'
 import LatestPressRelease from '@/components/LatestPressRelease'
 import Services from '@/components/Services'
 import ScrollToTopButton from '@/components/ScrollToTopButton'
+import type { Metadata } from 'next'
+
+
+export const metadata: Metadata = {
+  title: 'Congruence Market Insights - Market Research & Consulting',
+  description: 'Congruence is your trusted partner in navigating emerging opportunities and challenges through high-quality, tailored market research reports.',
+  keywords: 'Congruence Market Insights, CMI, Market Research, Industry Analysis, Market Analysis, Global Market Trends, Business Intelligence, Custom Market Research, Strategic Insights, Market Dynamics, Emerging Opportunities, Competitive Landscape, Data-driven Decision Making, Market Forecast, Industry Reports, Tailored Research Solutions, Market Intelligence Services',
+  alternates: {
+    canonical: `https://www.congruencemarketinsights.com/`,
+  },
+}
 
 export default function Home() {
   return (
-
     <section className="relative" >
       <h1 className='fixed text-transparent'>Congruence Market Insights</h1>
       <Search />
